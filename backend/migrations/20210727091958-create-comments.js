2dae4add254f2d770047e7647ddad8ddb109dad6
Theo Laurent <ze_king_to@hotmail.com>
2021-07-27 12:27:53 +0200

    migration vers BDD mysql

--- backend/migrations/20210727091958-create-comments.js
+++ backend/migrations/20210727091958-create-comments.js
@@ -1,43 +1,44 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('comments', {
+    await queryInterface.createTable("comments", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       userId: {
         allowNull: false,
         type: Sequelize.INTEGER,
-        references:{
-          model: 'Users',
-          key: 'id'
-        }
+        references: {
+          model: "Users",
+          key: "id",
+        },
       },
       postId: {
         allowNull: false,
         type: Sequelize.INTEGER,
-        references:{
-          model:"posts",
-          key:"id"
+        references: {
+          model: "posts",
+          key: "id",
+        },
       },
       message: {
         allowNull: false,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('comments');
-  }
+    await queryInterface.dropTable("comments");
+  },
 };
