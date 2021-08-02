f33f089cf09eafa06caf6d1ea37b6d9aa98196b7
Theo Laurent <ze_king_to@hotmail.com>
2021-08-02 16:03:13 +0200

    revu route users, protection injection SQL

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -13,9 +13,13 @@ exports.signup = function (req, res, next) {
         password: hash,
         bio: req.body.bio,
       };
-      db.query(`INSERT INTO user(firstName, lastName, email, password, bio)
-                VALUES("${user.firstName}","${user.lastName}","${user.email}","${user.password}","${user.bio}")`);
-      res.status(201).json({ message: "Utilisateur créé" });
+      db.query(`INSERT INTO user SET ?`, [user], function (error) {
+        if (error) {
+          res.status(400).json({ error });
+        } else {
+          res.status(201).json({ message: "Utilisateur créé." });
+        }
+      });
     })
     .catch(function (error) {
       res.status(500).json({ error });
@@ -23,35 +27,39 @@ exports.signup = function (req, res, next) {
 };
 
 exports.login = function (req, res, next) {
-  if (req.body.email != db.query(`SELECT email FROM user`)) {
-    return res.status(401).json({ error: "Utilisateur non trouvé !" });
-  } else {
-    bcrypt
-      .compare(
-        req.body.password,
-        db.query(`SELECT password FROM user WHERE email = "${req.body.email}"`)
-      )
-      .then(function (valid) {
-        if (!valid) {
-          return res.status(401).json({ error: "Mot de passe incorrect" });
+  db.query(
+    `SELECT id,password FROM user WHERE email = ?`,
+    [req.body.email],
+    function (error, results) {
+      if (error) {
+        throw error;
+      } else {
+        console.log(results);
+        if (results.length === 0) {
+          res.status(400).json({ message: "Utilisateur non trouvé." });
         } else {
-          res.status(200).json({
-            userId: db.query(
-              `SELECT id FROM user WHERE email = "${req.body.email}"`
-            ),
-            token: jwt.sign(
-              {
-                userId: db.query(
-                  `SELECT id FROM user WHERE email = "${req.body.email}"`
-                ),
-              },
-              "6b9adNtSEFFY5ZID6rRFHZ4FWnOMVr",
-              {
-                expiresIn: "24h",
+          bcrypt
+            .compare(req.body.password, results[0].password)
+            .then(function (valid) {
+              if (!valid) {
+                return res
+                  .status(401)
+                  .json({ message: "Mot de passe incorrect" });
+              } else {
+                res.status(200).json({
+                  userId: user[0].id,
+                  token: jwt.sign(
+                    {
+                      userId: user[0].id,
+                    },
+                    "6b9adNtSEFFY5ZID6rRFHZ4FWnOMVr",
+                    { expiresIn: "24h" }
+                  ),
+                });
               }
-            ),
-          });
+            });
         }
-      });
-  }
+      }
+    }
+  );
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require("../controllers/user");
 
 router.get("/signup", userController.signup);
+router.get("/login", userController.login);
 
 module.exports = router;
